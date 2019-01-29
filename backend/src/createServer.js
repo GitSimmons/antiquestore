f38eee2f942a267c150bf7ee51c0ad7e7e506492
GitSimmons <ben-simmons@mail.com>
2019-01-29 17:31:01 -0500

    Setup GraphQL Yoga server

---
+++ backend/src/createServer.js
@@ -0,0 +1,23 @@
+const { GraphQLServer } = require('graphql-yoga')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const db = require('./db')
+
+const typeDefs = 'src/schema.graphql'
+const resolvers = {
+  Query,
+  Mutation
+}
+
+function createServer () {
+  return new GraphQLServer({
+    typeDefs,
+    resolvers,
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => ({ ...req, db })
+  })
+}
+
+module.exports = createServer
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,10 @@
 require('dotenv').config({ path: 'variables.env' })
+const createServer = require('./createServer')
+
+const server = createServer()
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+}, (props) => console.log(`GQL Server is running on http://localhost:${props.port}`))
