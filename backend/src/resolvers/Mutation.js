a26d14f8900d9c9f83f4b861c5514cf3e1cd718f
GitSimmons <ben-simmons@mail.com>
2019-01-29 19:14:55 -0500

    Remove npm-run-all and change the scripts accordingly. Add a graphQL mutation for testing that everything is running as intended.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,16 @@
 const Mutation = {
-
+  async createItem (parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: { ...args }
+    })
+    return item
+  },
+  async deleteItem (parent, args, ctx, info) {
+    const item = await ctx.db.mutation.deleteItem({
+      where: { ...args }
+    })
+    return item
+  }
 }
 
 module.exports = Mutation
