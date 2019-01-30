51a0f37029c36360da96556c595e608f8afc9ce3
GitSimmons <ben-simmons@mail.com>
2019-01-29 19:41:40 -0500

    Add apollo-boost, next-with-apollo, graphql dependencies for frontend and setup some boilerplate fore them

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import { ApolloProvider } from 'react-apollo'
 import App, { Container } from 'next/app'
 import Page from '../components/Page'
-export default class myApp extends App {
+import withApollo from '../lib/withApollo'
+
+class myApp extends App {
+  static async getInitialProps ({ Component, ctx }) {
+    let pageProps = {}
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    // this exposes the query to the user
+    return { pageProps }
+  }
   render () {
-    const { Component } = this.props
+    const { Component, apollo, pageProps } = this.props
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     )
   }
 }
+export default withApollo(myApp)
