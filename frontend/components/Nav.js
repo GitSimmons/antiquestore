a042a1f8822428995ae2690ec485a402f644c939
GitSimmons <ben-simmons@mail.com>
2019-01-31 13:10:21 -0500

    Convert Nav from RCC to SFC with hooks

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Router from 'next/router'
 import { Image, Menu } from 'semantic-ui-react'
 import styled from 'styled-components'
 // Menu example from semantic ui docs
 
 const topMenuItems = [
-  {key: 'Sell', name: 'Sell', a:'/sell'},
-  {key: 'Cart', name: 'Cart'},
-  {key: 'Account', name: 'Account' },
-  {key: 'Sign Out', name: 'Sign Out'}
+  { key: 'Sell', name: 'Sell', a: '/sell' },
+  { key: 'Cart', name: 'Cart' },
+  { key: 'Account', name: 'Account' },
+  { key: 'Sign Out', name: 'Sign Out' }
 ]
 
 const items = [
@@ -16,7 +16,7 @@ const items = [
   { key: 'Estate Sales', name: 'Estate Sales', a: '/sales' },
   { key: 'Upcoming Events', name: 'Upcoming Events', a: '/events' },
   { key: 'Our History', name: 'Our History', a: '/about' },
-  { key: 'Consign With Us', name: 'Consign With Us', a: '/consign' },
+  { key: 'Consign With Us', name: 'Consign With Us', a: '/consign' }
 ]
 
 const StyledMenu = styled.div`
@@ -33,27 +33,22 @@ const StyledNav = styled.div`
   margin-right: 35px;
 `
 
-export default class MenuExampleBasic extends Component {
-  state = {
-    activeItem: ""
-  }
+const Nav = () => {
+  const [activeItem, setActiveItem] = useState()
 
-  handleItemClick = (e, { name, a }) => a && Router.push(a)
-
-  render() {
-    const { activeItem } = this.state
-    return (
-      <StyledMenu>
+  const handleItemClick = (e, { name, a }) => a && Router.push(a)
+  return (
+    <StyledMenu>
       <Menu inverted borderless>
         <Menu.Item header>
-          <Image src="/static/logo.png"/>
+          <Image src='/static/logo.png' />
         </Menu.Item>
         <StyledNav>
-        <div> <Menu borderless text secondary floated="right" inverted items={topMenuItems} size="small" onItemClick={this.handleItemClick} /> </div>
-        <div> <Menu borderless defaultActiveIndex="0" floated="right" inverted items={items} size="large" onItemClick={this.handleItemClick} /> </div>
+          <div> <Menu borderless text secondary floated='right' inverted items={topMenuItems} size='small' onItemClick={handleItemClick} /> </div>
+          <div> <Menu borderless defaultActiveIndex='0' floated='right' inverted items={items} size='large' onItemClick={handleItemClick} /> </div>
         </StyledNav>
       </Menu>
-      </StyledMenu>
-    )
-  }
+    </StyledMenu>
+  )
 }
+export default Nav
