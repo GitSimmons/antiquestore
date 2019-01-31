7650d82d9b43e998b31661084cbfffcd34132614
GitSimmons <ben-simmons@mail.com>
2019-01-31 17:35:54 -0500

    Add upload image function to the CreateItem page.

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,7 +1,7 @@
 import { gql } from 'apollo-boost'
 import { Mutation } from 'react-apollo'
 import { useState } from 'react'
-import { Button, Form } from 'semantic-ui-react'
+import { Button, Form, Label, Input, Icon, Image } from 'semantic-ui-react'
 import Router from 'next/router'
 
 const CREATE_ITEM_MUTATION = gql`
@@ -9,11 +9,13 @@ mutation CreateItem(
   $title: String!
   $description: String!
   $price: Int
+  $image: String
 ) {
   createItem(
     title: $title
     description: $description
     price: $price
+    image: $image
   ) {
     id
     title
@@ -26,12 +28,30 @@ const CreateItem = () => {
   const [price, setPrice] = useState(0)
   const [description, setDescription] = useState()
   const [loading, setLoading] = useState(false)
+  const [image, setImage] = useState()
+  const [largeImage, setLargeImage] = useState()
+
+  const uploadFile = async (file) => {
+    const url = 'https://api.cloudinary.com/v1_1/acloudforben/image/upload'
+    const data = new FormData()
+    data.append('file', file)
+    // TODO: Create more appropriate upload preset
+    data.append('upload_preset', 'sickfits')
+    const res = await fetch(url,
+      {
+        method: 'POST',
+        body: data
+      })
+    const { secure_url } = await res.json()
+    console.log(secure_url)
+    setImage(secure_url)
+  }
 
   const handleSubmit = async (createItem) => {
-    console.log({ title, price, description })
+    console.log({ title, price, description, image })
     setLoading(true)
     await createItem({ variables: {
-      title, price, description
+      title, price, description, image
     } })
     Router.push('/')
   }
@@ -41,7 +61,17 @@ const CreateItem = () => {
         <Form onSubmit={() => handleSubmit(createItem)} loading={loading}>
           <Form.Input required label='Title' placeholder='Title' onChange={(e) => setTitle(e.target.value)} />
           <Form.TextArea required label='Description' placeholder='A brief description of the object' onChange={(e) => setDescription(e.target.value)} type='text' />
-          <Form.Input label='Price' placeholder='Price of the object (in CAD)' onChange={(e) => setPrice(e.target.value * 100)} control='input' type='number' />
+          <Form.Field>
+            <Input labelPosition='right' type='text' placeholder='Amount (in CAD)' onChange={(e) => setPrice(e.target.value * 100)}>
+              <Label basic>$</Label>
+              <input />
+              <Label>.00</Label>
+            </Input>
+          </Form.Field>
+          <Form.Field>
+            {image && <Image src={image} size='medium' centered />}
+            <input id='upload-image-input' type='file' placeholder='file' onChange={(e) => uploadFile(e.target.files[0])} />
+          </Form.Field>
           <Form.Button primary>Submit</Form.Button>
         </Form>
       }
