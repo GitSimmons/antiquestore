bba00700ffa56f1cd2cb5f2839fa4f37357d6b5e
GitSimmons <ben-simmons@mail.com>
2019-01-30 21:17:03 -0500

    Add GQL logic for displaying single item page based on id!

---
+++ frontend/components/SingleItem.js
@@ -0,0 +1,29 @@
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
+
+const SINGLE_ITEM_QUERY = gql`
+  query SingleItem($id: ID!) {
+    item(where:{id: $id}) {
+      title
+      description
+    }
+  }
+`
+const SingleItem = props => (
+  <Query
+    query={SINGLE_ITEM_QUERY}
+    variables={{ id: props.id }}
+  >
+    {({ data, error, loading }) => {
+      if (loading) return null
+      if (error) return `Error!: ${error}`
+      return (
+        <div>
+          <h3>{data.item.title}</h3>
+          <span>{data.item.description}</span>
+        </div>
+      )
+    }}
+  </Query>
+)
+export default SingleItem
--- frontend/pages/item.js
+++ frontend/pages/item.js
@@ -1,6 +1,8 @@
+import SingleItem from '../components/SingleItem'
+
 export default (props) => (
   <div>
     <h1>Single item page</h1>
-    {props.query.id && <h1>{props.query.id}</h1>}
+    <SingleItem id={props.query.id} />
   </div>
 )
