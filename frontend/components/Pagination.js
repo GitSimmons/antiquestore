f73f914a3c09213b5f675156b057e5202c090227
GitSimmons <ben-simmons@mail.com>
2019-02-02 14:23:51 -0500

    Pagination component now routes to /page?=xx

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,9 +1,42 @@
 import React from 'react'
 import { Pagination } from 'semantic-ui-react'
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
+import { perPage } from '../lib/config'
+import Router from 'next/router'
+import Head from 'next/head'
 
-const PaginationComponent = () => (
+const PAGINATION_QUERY = gql`
+query PaginationQuery {
+  itemsConnection {
+    aggregate {
+      count
+    }
+  }
+}
+`
+const pageChangeHandler = (e, data) => Router.push({
+  pathname: '/',
+  query: { page: data.activePage }
+})
+
+const PaginationComponent = (props) => (
   <>
-    <Pagination defaultActivePage={1} totalPages={2} />
+    <Query query={PAGINATION_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading) return <div>Loading...</div>
+        const { count } = data.itemsConnection.aggregate
+        const pages = Math.ceil(count / perPage)
+        return (
+          <>
+            <Head>
+              <title>Aunt Sadie's Antiques - Page {props.page} of {pages}</title>
+            </Head>
+            <Pagination defaultActivePage={props.page} totalPages={pages} onPageChange={pageChangeHandler} />
+          </>
+        )
+      }}
+    </Query>
   </>
 )
 
