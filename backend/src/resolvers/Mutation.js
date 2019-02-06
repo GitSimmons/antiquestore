0e9b7d7b93d6abb4bb5ad78d919aace658af6ce9
GitSimmons <ben-simmons@mail.com>
2019-02-06 18:03:17 -0500

    Add User datamodel, and some simple query & mutation setups for login. Still need to add cookies

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 const Mutation = {
   async createItem (parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem({
@@ -19,6 +21,28 @@ const Mutation = {
       data
     })
     return item
+  },
+  async createUser (parent, args, ctx, info) {
+    const saltRounds = 10
+    args.email = args.email.toLowerCase()
+    const hash = await bcrypt.hash(args.password, saltRounds)
+    const user = await ctx.db.mutation.createUser({
+      data: { ...args, password: hash, permissions: { set: ['USER'] } }
+    })
+    return user
+  },
+  async signIn (parent, { email, password }, ctx, info) {
+    const user = await ctx.db.query.user({
+      where: { email }
+    })
+    if (!user) {
+      throw new Error(`No user found for email: ${email}`)
+    }
+    const valid = await bcrypt.compare(password, user.password)
+    if (!valid) {
+      throw new Error('Invalid Password!')
+    }
+    return user
   }
 }
 
