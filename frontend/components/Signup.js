9de3587c01e00ce22286ff1e229b354f349d17ce
GitSimmons <ben-simmons@mail.com>
2019-02-22 14:40:19 -0500

    Add signup component and page

---
+++ frontend/components/Signup.js
@@ -0,0 +1,43 @@
+import { gql } from 'apollo-boost'
+import { Mutation } from 'react-apollo'
+import { Form } from 'semantic-ui-react'
+import { useState } from 'react'
+
+const SIGNUP_MUTATION = gql`
+mutation createUser($name: String, $email: String, $password: String) {
+  createUser(
+    name: $name
+    email: $email
+    password: $password
+  ) {
+    id
+  }
+}
+`
+
+const SignUp = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const handleSubmit = async (signUpMutation) => {
+    await signUpMutation({ variables: {
+      name, email, password
+    } }).then((data) => console.log(data))
+  }
+  return (
+    <Mutation mutation={SIGNUP_MUTATION}>
+      {(signUpMutation) => (<div>
+        {`${name}, ${password}, ${email}`}
+        <Form>
+          <Form.Input label='name' onChange={(e) => setName(e.target.value)} />
+          <Form.Input label='email' onChange={(e) => setEmail(e.target.value)} />
+          <Form.Input label='password' onChange={(e) => setPassword(e.target.value)} />
+          <Form.Button primary onClick={() => handleSubmit(signUpMutation)}>Sign up</Form.Button>
+        </Form>
+      </div>)}
+
+    </Mutation>
+  )
+}
+
+export default SignUp
---
+++ frontend/pages/signup.js
@@ -0,0 +1,5 @@
+import SignUp from '../components/Signup'
+
+const Page = () => <div><SignUp /></div>
+
+export default Page
