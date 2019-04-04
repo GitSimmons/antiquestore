89227961832dda48dd06ebc632f1c4b57f3a5846
GitSimmons <ben-simmons@mail.com>
2019-04-03 21:27:54 -0400

    Add a User higher order component for fetching current user info

---
+++ frontend/components/User.js
@@ -0,0 +1,29 @@
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
+
+const CURRENT_USER_QUERY = gql`
+  query CURRENT_USER_QUERY {
+    currentUser {
+      id
+      email
+      name
+      permissions
+    }
+  }
+`
+
+const User = (props) => (
+  <Query {...props} query={CURRENT_USER_QUERY}>
+    {({ loading, error, data, refetch }) => {
+      return (
+        <>
+          {props.children(data)}
+        </>
+      )
+    }
+    }
+  </Query>
+)
+
+export default User
+export { CURRENT_USER_QUERY }
