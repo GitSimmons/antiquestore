f6a8e37417a44ac96f34451ac9d947ff4bc2e61f
GitSimmons <ben-simmons@mail.com>
2019-04-03 21:26:38 -0400

    Add frontend password reset support

---
+++ frontend/components/Reset.js
@@ -0,0 +1,42 @@
+import { gql } from 'apollo-boost'
+import { Mutation } from 'react-apollo'
+import { Form } from 'semantic-ui-react'
+import { useState } from 'react'
+import Router from 'next/router'
+const RESET_PASSWORD_MUTATION = gql`
+mutation resetPassword($password: String!, $confirmPassword: String!, $resetToken: String!) {
+  resetPassword(
+    password: $password
+    confirmPassword: $confirmPassword
+    resetToken: $resetToken
+  ) {
+    id
+  }
+}
+`
+
+const ResetPassword = ({ resetToken }) => {
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [password, setPassword] = useState('')
+  const handleSubmit = async (resetPasswordMutation) => {
+    await resetPasswordMutation({ variables: {
+      password,
+      confirmPassword,
+      resetToken
+    } }).then((data) => Router.push('/'))
+  }
+  return (
+    <Mutation mutation={RESET_PASSWORD_MUTATION}>
+      {(resetPasswordMutation) => (
+        <Form>
+          <Form.Input type='password' label='password' onChange={(e) => setPassword(e.target.value)} />
+          <Form.Input type='password' label='confirm your password' onChange={(e) => setConfirmPassword(e.target.value)} />
+          <Form.Button primary onClick={() => handleSubmit(resetPasswordMutation)}>Reset Password</Form.Button>
+        </Form>
+      )}
+
+    </Mutation>
+  )
+}
+
+export default ResetPassword
