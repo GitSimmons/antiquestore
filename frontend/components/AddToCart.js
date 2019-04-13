156af8dd886cec81aba43959c235e84b6a7facba
GitSimmons <ben-simmons@mail.com>
2019-04-13 12:09:45 -0400

    Create an add to cart button component

---
+++ frontend/components/AddToCart.js
@@ -0,0 +1,25 @@
+import { Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
+import { Button } from 'semantic-ui-react'
+import { TOGGLE_CART_MUTATION } from './Cart.js'
+
+const ADD_TO_CART_MUTATION = gql`
+  mutation ADD_TO_CART_MUTATION($id: ID!) {
+    addToCart (id: $id) {
+      id
+    }
+}
+`
+
+const AddToCart = ({ id }) => {
+  return (
+    <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
+      {addToCartMutation =>
+        <Button negative circular icon='cart' floated='right' onClick={addToCartMutation} />
+      }
+    </Mutation>
+
+  )
+}
+
+export default AddToCart
