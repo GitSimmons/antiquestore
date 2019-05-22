eb6e9dea919e0d107f681809d888a66430dd11f6
GitSimmons <ben-simmons@mail.com>
2019-05-22 10:56:59 -0400

    Refactor Item Cards into their own component

---
+++ frontend/components/ItemCard/ItemCard.js
@@ -0,0 +1,78 @@
+import { gql } from 'apollo-boost'
+import { Mutation } from 'react-apollo'
+import { Button, Card, Image, Modal, Header, Icon } from 'semantic-ui-react'
+import { ConvertToCurrency } from '../../lib/utils'
+import AddToCart from '../AddToCart'
+import UpdateItem from '../UpdateItem'
+
+const DELETE_ITEM_MUTATION = gql`
+mutation DELETE_ITEM_MUTATION($id: ID!) {
+  deleteItem(id: $id) {
+    id
+  }
+}
+`
+const ItemCard = ({ item, refetch }) => {
+  console.log(item)
+  return (
+    <Mutation
+      mutation={DELETE_ITEM_MUTATION}
+      key={item.id}
+      refetchQueries={[
+        'ALL_ITEMS_QUERY',
+        'PAGINATION_QUERY']}
+      variables={{ id: item.id }}
+    >
+      {deleteItem =>
+        <Card key={item.id}>
+          {item.image
+            ? <Image src={item.image} />
+            : <Image src='/static/image.png' />}
+          <Card.Content>
+            <Card.Header>{item.title}</Card.Header>
+            {item.price > 0 && <Card.Meta> <span>{ConvertToCurrency(item.price)}</span></Card.Meta>}
+            {item.description && <Card.Description>{item.description}</Card.Description>}
+          </Card.Content>
+          <Card.Content extra>
+            <AddToCart id={item.id} item={item} />
+            <Modal
+              basic
+              closeIcon
+              dimmer='blurring'
+              size='small'
+              trigger={
+                <Button negative circular icon='trash' floated='right' />
+              }
+            >
+              <Header icon='trash' content='Delete Item' />
+              <Modal.Content>
+                <p>
+            Are you sure you want to delete {item.title} ?
+                </p>
+              </Modal.Content>
+              <Modal.Actions >
+                <Button basic color='red' inverted>
+                  <Icon name='remove' /> No
+                </Button>
+                <Button color='green' inverted onClick={async () => {
+                  await deleteItem()
+                  refetch()
+                }}>
+                  <Icon name='checkmark' /> Yes
+                </Button>
+              </Modal.Actions>
+            </Modal>
+            <Modal trigger={<Button primary circular icon='pencil' floated='right' />} dimmer='blurring'>
+              <Modal.Header>{item.title}</Modal.Header>
+              <Modal.Content image>
+                <Image wrapped size='medium' src={item.image} />
+                <UpdateItem id={item.id} />
+              </Modal.Content>
+            </Modal>
+          </Card.Content>
+        </Card>}
+    </Mutation>
+  )
+}
+
+export default ItemCard
--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -1,13 +1,10 @@
 import { gql } from 'apollo-boost'
-import { Query, Mutation } from 'react-apollo'
-import { Button, Card, Image, Modal, Header, Icon } from 'semantic-ui-react'
-import Router from 'next/router'
-import { ConvertToCurrency } from '../lib/utils'
+import { Query } from 'react-apollo'
+import { Card } from 'semantic-ui-react'
 import Pagination from './Pagination'
 import { perPage } from '../lib/config'
-import AddToCart from './AddToCart'
-import SingleItem from './SingleItem'
-import UpdateItem from './UpdateItem'
+
+import ItemCard from './ItemCard/ItemCard'
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY(
@@ -27,13 +24,6 @@ const ALL_ITEMS_QUERY = gql`
     }
   }
 `
-const DELETE_ITEM_MUTATION = gql`
-  mutation DELETE_ITEM_MUTATION($id: ID!) {
-    deleteItem(id: $id) {
-      id
-    }
-  }
-`
 
 const Items = (props) => {
   const page = parseFloat(props.page)
@@ -50,67 +40,10 @@ const Items = (props) => {
           <Card.Group centered stackable>
             {data.items.map(
               (item) =>
-                <Mutation
-                  mutation={DELETE_ITEM_MUTATION}
-                  key={item.id}
-                  refetchQueries={[
-                    'ALL_ITEMS_QUERY',
-                    'PAGINATION_QUERY']}
-                  variables={{ id: item.id }}
-                >
-                  {deleteItem =>
-                    <Card key={item.id}>
-                      {item.image
-                        ? <Image src={item.image} />
-                        : <Image src='/static/image.png' />}
-                      <Card.Content>
-                        <Card.Header>{item.title}</Card.Header>
-                        {item.price > 0 && <Card.Meta> <span>{ConvertToCurrency(item.price)}</span></Card.Meta>}
-                        {item.description && <Card.Description>{item.description}</Card.Description>}
-                      </Card.Content>
-                      <Card.Content extra>
-                        <AddToCart id={item.id} item={item} />
-                        <Modal
-                          basic
-                          closeIcon
-                          dimmer='blurring'
-                          size='small'
-                          trigger={
-                            <Button negative circular icon='trash' floated='right' />
-                          }
-                        >
-                          <Header icon='trash' content='Delete Item' />
-                          <Modal.Content>
-                            <p>
-                              Are you sure you want to delete {item.title} ?
-                            </p>
-                          </Modal.Content>
-                          <Modal.Actions >
-                            <Button basic color='red' inverted>
-                              <Icon name='remove' /> No
-                            </Button>
-                            <Button color='green' inverted onClick={async () => {
-                              await deleteItem()
-                              refetch()
-                            }}>
-                              <Icon name='checkmark' /> Yes
-                            </Button>
-                          </Modal.Actions>
-                        </Modal>
-                        <Modal trigger={<Button primary circular icon='pencil' floated='right' />} dimmer='blurring'>
-                          <Modal.Header>{item.title}</Modal.Header>
-                          <Modal.Content image>
-                            <Image wrapped size='medium' src={item.image} />
-                            <UpdateItem id={item.id} />
-                          </Modal.Content>
-                        </Modal>
-                      </Card.Content>
-                    </Card>}
-                </Mutation>
+                <ItemCard item={item} key={item.id} refetch={refetch} />
             )}
           </Card.Group>
           <Pagination page={page} refetch={refetch} />
-
         </>
         )
       }}
