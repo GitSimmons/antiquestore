19dc107adeb1a54ed93107baabab3e3c5447c63c
GitSimmons <ben-simmons@mail.com>
2019-06-07 12:35:05 -0400

    Remove extraneous heading from Single item Page

--- frontend/pages/item.js
+++ frontend/pages/item.js
@@ -1,8 +1,5 @@
 import SingleItem from '../components/SingleItem'
 
 export default (props) => (
-  <div>
-    <h1>Single item page</h1>
-    <SingleItem id={props.query.id} />
-  </div>
+  <SingleItem id={props.query.id} />
 )
