cf82a28e59638392fa12a3f6401e173c261d1fb3
GitSimmons <ben-simmons@mail.com>
2019-06-07 12:35:54 -0400

    Change ItemCard to display a mock item while loading

--- frontend/components/ItemCard/ItemCard.js
+++ frontend/components/ItemCard/ItemCard.js
@@ -1,10 +1,24 @@
-import { Card, Image } from 'semantic-ui-react'
-import { ConvertToCurrency } from '../../lib/utils'
+import { Button, Card, Dimmer, Image, Loader, Segment } from 'semantic-ui-react'
+import { convertToCurrency } from '../../lib/utils'
 import AddToCart from '../AddToCart'
 import UpdateItemButton from './UpdateItemButton'
 import DeleteItemButton from './DeleteItemButton'
 
 const ItemCard = ({ item, refetch }) => {
+  if (!item) {
+    return (
+      <Card>
+        <Image src='/static/image.png' />
+        <Card.Content>
+          <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+        </Card.Content>
+        <Card.Content extra>
+          <Button circular icon='cart' floated='right' disabled />
+          <Button circular icon='trash' floated='right' disabled />
+          <Button circular icon='pencil' floated='right' disabled />
+        </Card.Content>
+      </Card>)
+  }
   return (
     <Card key={item.id}>
       {item.image
@@ -12,7 +26,7 @@ const ItemCard = ({ item, refetch }) => {
         : <Image src='/static/image.png' />}
       <Card.Content>
         <Card.Header>{item.title}</Card.Header>
-        {item.price > 0 && <Card.Meta> <span>{ConvertToCurrency(item.price)}</span></Card.Meta>}
+        {item.price > 0 && <Card.Meta> <span>{convertToCurrency(item.price)}</span></Card.Meta>}
         {item.description && <Card.Description>{item.description}</Card.Description>}
       </Card.Content>
       <Card.Content extra>
