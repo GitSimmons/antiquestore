a17b90ef19ef0e8bda6a5cff78be4e80d2d86beb
GitSimmons <ben-simmons@mail.com>
2019-06-10 10:43:19 -0400

    Add styles to Item cards so that they better scale in mobile resolutions

--- frontend/components/ItemCard/ItemCard.js
+++ frontend/components/ItemCard/ItemCard.js
@@ -3,12 +3,26 @@ import { convertToCurrency } from '../../lib/utils'
 import AddToCart from '../AddToCart'
 import UpdateItemButton from './UpdateItemButton'
 import DeleteItemButton from './DeleteItemButton'
+import Router from 'next/router'
+import styled from 'styled-components'
 
+const StyledImage = styled(Image)`
+  height: 200px !important;
+  object-fit: cover;
+  
+  @media (max-width: 767px) {
+    height: auto!important;
+    object-fit: fill;
+  }
+`
 const ItemCard = ({ item, refetch }) => {
+  const routeToItemPage = (id) => {
+    Router.push(`/item?id=${id}`)
+  }
   if (!item) {
     return (
-      <Card>
-        <Image src='/static/image.png' />
+      <Card style={{ maxWidth: '90vw' }}>
+        <StyledImage src='/static/image.png' />
         <Card.Content>
           <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
         </Card.Content>
@@ -20,12 +34,12 @@ const ItemCard = ({ item, refetch }) => {
       </Card>)
   }
   return (
-    <Card key={item.id}>
+    <Card style={{ maxWidth: '90vw' }}key={item.id}>
       {item.image
-        ? <Image src={item.image} />
-        : <Image src='/static/image.png' />}
+        ? <StyledImage fluid src={item.image} onClick={(e) => routeToItemPage(item.id)} />
+        : <StyledImage src='/static/image.png' />}
       <Card.Content>
-        <Card.Header>{item.title}</Card.Header>
+        <Card.Header onClick={(e) => routeToItemPage(item.id)}>{item.title}</Card.Header>
         {item.price > 0 && <Card.Meta> <span>{convertToCurrency(item.price)}</span></Card.Meta>}
         {item.description && <Card.Description>{item.description}</Card.Description>}
       </Card.Content>
