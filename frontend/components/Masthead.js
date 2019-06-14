f5a0b0f3d283297f5da369903409bde9d17a3d7b
GitSimmons <ben-simmons@mail.com>
2019-06-14 10:43:11 -0400

    Change Masthead to be self-contained and no longer a carousel

--- frontend/components/Masthead.js
+++ frontend/components/Masthead.js
@@ -1,6 +1,8 @@
-import Carousel from './Carousel/Carousel'
 
-const images = [
+import { Button, Icon, Header } from 'semantic-ui-react'
+import styled from 'styled-components'
+
+const mastheadObject =
   {
     title: `Chandeliers`,
     description: `Looking for something fancier than an edison bulb, but less fancy than recessed lighting?`,
@@ -9,19 +11,47 @@ const images = [
     src: `https://res.cloudinary.com/acloudforben/image/upload/c_scale,f_auto,w_1980,q_80/v1559858462/auntsadies/chandelier`,
     headerColor: '',
     descColor: ''
-  },
-  {
-    title: `Collectibles`,
-    description: `Find the perfect gift that's too valuable for them to throw away.`,
-    credit: `Andrej Ivanov`,
-    backgroundColor: 'black',
-    src: `https://res.cloudinary.com/acloudforben/image/upload/c_scale,f_auto,w_1980,q_80/v1559926068/auntsadies/hero1`,
-    a: 'https://andrejiphotography.com/'
   }
-]
 
-const Masthead = ({ children }) => (
-  <Carousel images={images} initialStep={0} />
-)
+const Masthead = () => {
+  const CarouselContrastTriangleThing = styled.div`
+  left: -45vw;
+  top: 0;
+  position: absolute;
+  height: 100%;
+  min-width: 80%;
+  background-color: ${mastheadObject.backgroundColor || '#bab9ff'};
+  transform: skewX(-25deg);
+  opacity: 0.25;
+`
+  const ImageContainer = styled.div`
+  display: flex;
+  position: absolute;
+  left: 0;
+  padding-left: 2rem;
+  align-items: center;
+  min-height: 650px;
+  width: 100vw; 
+  background-image: url(${mastheadObject.src});
+  background-attachment: fixed;
+  background-position: top;
+`
+
+  return (
+    <div style={{ height: '650px' }}>
+      <ImageContainer>
+        <CarouselContrastTriangleThing />
+        <div>
+          <Header as='h1' style={{ maxWidth: '65%', fontSize: '6rem' }} inverted>{mastheadObject.title}
+            <Header.Subheader>
+              {mastheadObject.description}
+            </Header.Subheader>
+            <Button color='blue'>View Collection</Button>
+          </Header>
+        </div>
+      </ImageContainer>
+    </div>
+  )
+}
 
 export default Masthead
