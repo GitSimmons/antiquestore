1254beeab20d4bc1a950c0d8993a37ad403ead94
Ben Simmons <ben.ryan.simmons@gmail.com>
2020-06-15 11:03:10 -0400

    fix: a bug where the contrast triangle on the masthead wouldn't hide its overflow on high resolutions

--- frontend/components/Masthead.js
+++ frontend/components/Masthead.js
@@ -13,7 +13,24 @@ const mastheadObject = {
 
 const Masthead = () => {
   const CarouselContrastTriangleThing = styled.div`
-    left: -45vw;
+  `;
+  const ImageContainer = styled.div`
+    position: relative;
+    display: flex;
+    padding-left: 2rem;
+    align-items: center;
+    height: 650px;
+    max-height: 100vh;
+    overflow: hidden;
+    max-width: 1280px;
+    background-image: url(${mastheadObject.src});
+    background-attachment: fixed;
+    background-position: top;
+
+    /* after pseudo element to create the contrastTriangle */
+
+    &::after {
+    left: -50%;
     top: 0;
     position: absolute;
     height: 650px;
@@ -22,18 +39,8 @@ const Masthead = () => {
     min-width: 80%;
     background-color: ${mastheadObject.backgroundColor || `rgba(186,185,255, 0.12)`};
     transform: skewX(-25deg);
-  `;
-  const ImageContainer = styled.div`
-    display: flex;
-    padding-left: 2rem;
-    align-items: center;
-    height: 650px;
-    max-height: 100vh;
-    overflow: hidden;
-    max-width: 1400px;
-    background-image: url(${mastheadObject.src});
-    background-attachment: fixed;
-    background-position: top;
+    content: '';
+    }
   `;
 
   return (
